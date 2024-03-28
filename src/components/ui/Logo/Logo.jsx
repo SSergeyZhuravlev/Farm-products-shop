373c315c5c7a3221a8b93d079c68d5c70df8f757
Журавлев Сергей <serezha.zhuravlev.90@gmail.com>
2024-03-28 22:10:30 +0300

    deploy version

--- src/components/ui/Logo/Logo.jsx
+++ src/components/ui/Logo/Logo.jsx
@@ -1,9 +1,10 @@
+import farmerLogo from '/src/assets/images/farmer-logo.svg';
 import './Logo.css';
 
 export const Logo = () => {
     return (
         <div className="header__logo">
-            <img className="header__logo__img" src="/src/assets/images/farmer-logo.svg" alt="Изображение фермера" />
+            <img className="header__logo__img" src={farmerLogo} alt="Изображение фермера" />
             <p className="header__logo__text">Фермерские продукты</p>
         </div>
     )
--- src/mocks/profits.js
+++ src/mocks/profits.js
@@ -1,30 +1,35 @@
+import profitsEat from '/src/assets/images/profits-img-eat.png';
+import profitsGarbage from '/src/assets/images/profits-img-eat.png';
+import profitsSprout from '/src/assets/images/profits-img-eat.png';
+import profitsNoMeat from '/src/assets/images/profits-img-eat.png';
+
 export const profits = [
     {
         id: crypto.randomUUID(),
         productsType: 'farm',
         prop: 'Еда намного вкуснее',
         propDescr: 'Домашняя колбаса из мяса, соли и специй и колбаса из магазина — два настолько разных продукта, что они даже не родственники',
-        src: '/src/assets/images/profits-img-eat.png',
+        src: profitsEat,
     },
     {
         id: crypto.randomUUID(),
         productsType: 'store',
         prop: 'Просроченные продукты',
         propDescr: 'Из-за большого количество товара сотрудники магазинов не успевают своевременно производить замену товара',
-        src: '/src/assets/images/profits-img-garbage.png',
+        src: profitsGarbage,
     },
     {
         id: crypto.randomUUID(),
         productsType: 'farm',
         prop: 'Натуральные продукты',
         propDescr: 'Поставляем местные органические продукты выращенные без пестицидов и химических удобрений.',
-        src: '/src/assets/images/profits-img-sprout.png',
+        src: profitsSprout,
     },
     {
         id: crypto.randomUUID(),
         productsType: 'store',
         prop: 'Некачественное мясо',
         propDescr: 'Мясные полуфабрикаты, в которых содержится чрезмерно много натрия, вредных жиров, консервантов',
-        src: '/src/assets/images/profits-img-no-meat.png',
+        src: profitsNoMeat,
     },
 ];
